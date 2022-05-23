55ce1556d75584a3594f867eb03ed43db4fcab0a
Amrit Pandey <amritpandey@tuta.io>
2022-05-23 13:09:58 +0530

    Math: ncr program

---
+++ mathematics/ncr.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#define String string
+using namespace std;
+
+long ncr (int, int);
+
+int main() {
+    int n = 5;
+    int r = 2;
+    cout << ncr(5, 2) << endl;
+}
+
+long ncr(int n, int r) {
+    int res = 0;
+    if (r == 0) {
+        return 1;
+    }
+    return ncr(n, r - 1) * (n - r + 1) / r;
+}
