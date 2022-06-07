330fc1b4c34a57cc2c22482e71cecdd9e2d0944c
Amrit Pandey <amritpandey@tuta.io>
2022-06-07 20:40:50 +0530

    Problems: Hashing

---
+++ hashing/OpenAddressing.java
@@ -0,0 +1,201 @@
+package hashing;
+
+import java.util.*;
+import java.io.*;
+/*
+author: @ok-ape
+PS:
+*/
+
+/* test case:
+
+
+
+*/
+
+public class OpenAddressing {
+
+    public static void main(String[] args) {
+        // code here
+        int[] A = new int[7];
+        doubleHashingInsert(A, 49);
+        doubleHashingInsert(A, 63);
+        doubleHashingInsert(A, 56);
+        doubleHashingInsert(A, 52);
+        doubleHashingInsert(A, 54);
+        doubleHashingInsert(A, 48);
+        for (int n : A) {
+            System.out.print(n + " ");
+        }
+    }
+
+    public static void doubleHashingInsert(int[] A, int x) {
+        // h = (h1(x) + h2(x) * i) % m
+        // h1(x) = h1 % m
+        // h2(x) = 6 - x % 6 
+        boolean empty = false;
+        int i, h1, h2, m, t, h;
+        i = 1; m = 7; t = 6;
+        h1 = x % m;
+        h2 = t - (x % t);
+        while (!empty) {
+            if (A[h1] == 0) {
+                empty = true;
+                A[h1] = x;
+                continue;
+            }
+            h = (h1 + h2 * i) % m;
+            if (A[h] == 0) {
+                empty = true;
+                A[h] = x;
+            }
+            i++;
+        }
+    }
+
+
+
+
+    /* boilerplate code - not part of actual logic */
+    private static FastReader sc = new FastReader();
+
+    public static void swap(int[] A, int x, int y) {
+        int temp = A[x];
+        A[x] = A[y];
+        A[y] = temp;
+    }
+
+    static void print(Object c) {
+        System.out.println(c.toString());
+    }
+
+    static void print(int[] A) {
+        for (int n : A) {
+            System.out.print(n + " ");
+        }
+        System.out.println();
+    }
+
+    static class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+            this.left = null;
+            this.right = null;
+        }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        public static TreeNode generateSampleTree() {
+            TreeNode root = new TreeNode(10);
+            root.left = new TreeNode(20);
+            root.right = new TreeNode(30);
+            root.left.right = new TreeNode(50);
+            root.right.left = new TreeNode(40);
+            root.right.right = new TreeNode(8);
+            root.left.right.left = new TreeNode(78);
+            root.right.left.right = new TreeNode(-100);
+            return root;
+        }
+
+        public static TreeNode generateSampleBST() {
+            TreeNode root = new TreeNode(60);
+            root.left = new TreeNode(55);
+            root.right = new TreeNode(65);
+            root.left.left = new TreeNode(50);
+            root.left.right = new TreeNode(57);
+            root.right.right = new TreeNode(70);
+            root.right.right.right = new TreeNode(80);
+            return root;
+        }
+
+        public static void printTree(TreeNode root) {
+            if (root == null) {
+                return;
+            }
+            ArrayDeque<TreeNode> Q = new ArrayDeque<>();
+            Q.offer(root);
+            while (!Q.isEmpty()) {
+                int count = Q.size(); // important step
+                // do not use Q.size() in loop termination
+                // as Q.size() keep variying
+                for (int i = 0; i < count; i++) {
+                    TreeNode current = Q.poll();
+                    System.out.print(current.val + " ");
+                    if (current.left != null) {
+                        Q.offer(current.left);
+                    }
+                    if (current.right != null) {
+                        Q.offer(current.right);
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    static class Pair<T, S> {
+        T x;
+        S y;
+
+        public Pair(T x, S y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + this.x + ", " + this.y + ")";
+        }
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
