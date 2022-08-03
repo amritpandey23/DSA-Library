ad55d7cdcad9d46185971725223e1f4ebc152941
Amrit Pandey <amritpandey@tuta.io>
2022-08-04 00:29:07 +0530

    Math: power of two

---
+++ mathematics/isPowerOf2.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define print(a)	for (auto x : a) cout << x << " "; cout << endl;
+#define mod			1e9 + 7
+#define endl		"\n"
+
+/* 
+author: ok-ape 
+date: 
+*/
+
+/* test cases
+
+
+*/
+
+bool isPowerOfTwo(unsigned int n) {
+    return !(n & (n - 1));
+}
+
+void solve() {
+	// code here
+    cout << isPowerOfTwo(n) << endl;
+}
+
+int32_t main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt",  "r",  stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+	#endif
+
+	int t = 1;
+	// cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
